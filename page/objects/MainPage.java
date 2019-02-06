e92d817d976064a2813914d976db2cb01aecc40b
Olgierd <olgierdjacher@gmail.com>
2019-02-06 13:24:13 +0100

    lokatory

--- page/objects/MainPage.java
+++ page/objects/MainPage.java
@@ -3,6 +3,8 @@ package page.objects;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class MainPage {
     private  WebDriver driver;
@@ -11,9 +13,82 @@ public class MainPage {
         this.driver = driver;
     }
 
+
+     //Lokatory
+
+    @FindBy(xpath = "//*[@id=\"nav\"]/a[1]")
+    private WebElement stronaGlownaGornylewyRog;
+
+    @FindBy(xpath = "//*[@id=\"nav\"]/a[2]")
+    private WebElement strefaKlientGornyLewyRog;
+
+    @FindBy(xpath ="//*[@id=\"nav\"]/a[3]" )
+    private WebElement kontaktZNamiPrawyGornyRog;
+
+    @FindBy(xpath = "//*[@id=\"navi\"]/ul/li[1]/a")
+    private WebElement biuroRachunkoweGorneMenu;
+
+    @FindBy (xpath = "//*[@id=\"navi\"]/ul/li[2]/a")
+    private WebElement naszaOfertaGornemenu;
+
+    @FindBy (xpath = "//*[@id=\"navi\"]/ul/li[3]/a")
+    private WebElement promocjeGornemenu;
+
+    @FindBy (xpath = "//*[@id=\"navi\"]/ul/li[4]/a")
+    private WebElement kontaktGorneMenu;
+
+    @FindBy (xpath = "//*[@id=\"slider\"]/div[2]/a[1]")
+    private WebElement wiecejHeader;
+
+    @FindBy (xpath = "//*[@id=\"0\"]")
+    private WebElement jedenHeader;
+
+    @FindBy (xpath = "//*[@id=\"1\"]")
+    private WebElement dwaHeader;
+
+    @FindBy (xpath = "//*[@id=\"2\"]")
+    private WebElement trzyHeader;
+
+    @FindBy (xpath = "//*[@id=\"3\"]")
+    private WebElement czteryHeader;
+
+    @FindBy (xpath = "//*[@id=\"4\"]")
+    private WebElement piecHeader;
+
+    @FindBy (xpath = "//*[@id=\"foot1\"]/ul/li[1]/a")
+    private WebElement biuroRachunkoweDolnemenu;
+
+    @FindBy (xpath = "//*[@id=\"foot1\"]/ul/li[2]/a")
+    private WebElement biuroKsiegoweDolneMenu;
+
+    @FindBy (xpath = "//*[@id=\"foot1\"]/ul/li[3]/a")
+    private WebElement ofertaDolneMenu;
+
+    @FindBy (xpath = "//*[@id=\"foot1\"]/ul/li[4]/a")
+    private WebElement promocjeDolneMenu;
+
+    @FindBy (xpath = "//*[@id=\"foot1\"]/ul/li[5]/a")
+    private WebElement przydatneLinkiDolneMenu;
+
+    @FindBy (xpath ="//*[@id=\"foot1\"]/ul/li[6]/a")
+    private WebElement uslugiKsiegoweDolneMenu;
+
+    @FindBy (xpath = "//*[@id=\"foot1\"]/ul/li[7]/a")
+    private WebElement kontaktDolneMenu;
+
+    @FindBy (xpath = "//*[@id=\"foot2\"]/div/span[5]/a")
+    private WebElement emailDolnaStopka;
+
+    @FindBy (xpath = "//*[@id=\"foot2\"]/div/a")
+    private WebElement mapaStronyDolnaStopka;
+
+
+
+
+
+
     public void clickOnStrefaKlientaLink() {
-        WebElement strefaKlienta = driver.findElement(By.xpath("//*[@id=\"nav\"]/a[2]"));
-        strefaKlienta.click();
+        strefaKlientGornyLewyRog.click();
     }
 
 
---
+++ page/objects/PromocjePage.java
@@ -0,0 +1,63 @@
+package page.objects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class PromocjePage {
+
+
+        private WebDriver driver;
+
+        public PromocjePage(org.openqa.selenium.WebDriver driver) {
+            this.driver = driver;
+        }
+    //Lokatory
+
+    @FindBy (xpath = "//*[@id=\"cont\"]/div[1]/a/div")
+    private WebElement uslugiKsiegowe;
+
+    @FindBy (xpath = "//*[@id=\"cont\"]/div[2]/a/div")
+    private WebElement uslugiKadrowe;
+
+    @FindBy (xpath = "//*[@id=\"cont\"]/div[3]/a/div")
+    private WebElement ksiegowoscOnline;
+
+    @FindBy (xpath = "//*[@id=\"cont\"]/div[4]/a/div")
+    private WebElement ksiegowoscStowarzyszen;
+
+    @FindBy (xpath = "//*[@id=\"cont\"]/div[5]/a/div")
+    private WebElement controlling;
+
+    @FindBy (xpath = "//*[@id=\"cont\"]/div[6]/a/div")
+    private WebElement uslugiPrawne;
+
+    @FindBy (xpath = "//*[@id=\"cont\"]/div[7]/a/div")
+    private WebElement interesujacePromocje;
+
+    @FindBy (xpath = "//*[@id=\"cont\"]/div[8]/a/div")
+    private WebElement wirtualneBiuro;
+
+    @FindBy (xpath = "//*[@id=\"cont\"]/div[9]/a/div")
+    private WebElement szkoleniaBHP;
+
+    @FindBy (xpath = "//*[@id=\"cont\"]/div[10]/a/div")
+    private WebElement rozliczanieDotacji;
+
+    @FindBy (xpath = "//*[@id=\"cont\"]/div[11]/a/div")
+    private WebElement monitoringNaleznosci;
+
+    @FindBy (xpath = "//*[@id=\"cont\"]/div[11]/a/div")
+    private WebElement obslugaRachunku;
+
+    @FindBy (xpath = "//*[@id=\"cont\"]/div[13]/a/div")
+    private WebElement dzierzawaOprogramownia;
+
+    @FindBy (xpath = "//*[@id=\"panel\"]")
+    private WebElement strefaKlienta;
+
+    
+
+
+
+    }
